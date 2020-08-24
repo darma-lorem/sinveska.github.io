538d2356a607429fcc4b9c697bac62e9e3d4e600
darma <putradarma030@gmaill.com>
2020-08-24 18:15:26 +0800

    revisi 2

--- public/js/app.js
+++ public/js/app.js
@@ -37331,8 +37331,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\sinveska\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\sinveska\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\sidang\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\sidang\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
